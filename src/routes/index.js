c2d887e396a7194ec5ecd83daf93913b57d52dd1
ezequiel <molivera@labsxd.com>
2020-07-01 18:54:51 -0300

    cambie estilos para mobile

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const { Router } = require('express');
 const router = Router();
-const stripe = require('stripe')('private-key');
+const stripe = require('stripe')('sk_test_51H07BXL0Vs2q39zFP55Im78mWTtqDOmR0OgQYQ5q8sb2qOXXKVnZFfE5XOeq2BfTSWyqghR0YZherg78oJnqLs8i00dEvmIhLb');
 
 router.get('/', async (req, res) => {
     res.render('index')
